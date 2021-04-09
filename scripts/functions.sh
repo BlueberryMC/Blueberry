91aca40a6e8e96a5ae5b6759ef7fa9d682656c46
acrylic-style <ilyy@outlook.jp>
2021-04-09 21:45:09 +0900

    Update to 21w14a

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-export version=21w13a
+export version=21w14a
 export basedir="$(pwd -P)"
-export clientJarUrl="https://launcher.mojang.com/v1/objects/507be109a1902b7c3b30c1e900299344c7c0bee6/client.jar"
+export clientJarUrl="https://launcher.mojang.com/v1/objects/4af7eb4770d3e708d287db3bf023d4bbc60465b0/client.jar"
