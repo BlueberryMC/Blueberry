d2e9e9b5ba12ec96b9dc5e624dee2b99406a8e99
acrylic-style <me@acrylicstyle.xyz>
2021-12-15 19:34:00 +0900

    let's just stick to snapshot

--- buildSrc/src/main/kotlin/net/blueberrymc/gradle/buildSrc/constants/MagicConstants.kt
+++ buildSrc/src/main/kotlin/net/blueberrymc/gradle/buildSrc/constants/MagicConstants.kt
@@ -1,6 +1,6 @@
 package net.blueberrymc.gradle.buildSrc.constants
 
-const val apiVersion = "0.2.1"
+const val apiVersion = "0.2.1-SNAPSHOT"
 const val minecraftVersion = "1.18.1"
 const val text2speechVersion = "1.11.3"
 const val log4jVersion = "2.14.1"
