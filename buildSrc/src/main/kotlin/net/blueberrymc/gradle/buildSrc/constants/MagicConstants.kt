aff704e1a9c210809760a77f2f877e7c89018617
acrylic-style <me@acrylicstyle.xyz>
2021-12-21 00:17:25 +0900

    Update log4j to 2.17.0 (Fixes CVE-2021-45105)

--- buildSrc/src/main/kotlin/net/blueberrymc/gradle/buildSrc/constants/MagicConstants.kt
+++ buildSrc/src/main/kotlin/net/blueberrymc/gradle/buildSrc/constants/MagicConstants.kt
@@ -1,7 +1,7 @@
 package net.blueberrymc.gradle.buildSrc.constants
 
-const val apiVersion = "0.2.1-SNAPSHOT"
+const val apiVersion = "0.2.2-SNAPSHOT"
 const val minecraftVersion = "1.18.1"
 const val text2speechVersion = "1.11.3"
-const val log4jVersion = "2.14.1"
+const val log4jVersion = "2.17.0" // log4j sucks
 const val lwjglVersion = "3.2.2"
