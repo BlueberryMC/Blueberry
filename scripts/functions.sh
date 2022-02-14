9f3b05002f8195bd7d19325e89d2d72179309bf3
acrylic-style <me@acrylicstyle.xyz>
2022-02-15 02:31:20 +0900

    Fix small typo

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-export version=21w06a
+export version=22w06a
 export basedir="$(pwd -P)"
 export clientJarUrl="https://launcher.mojang.com/v1/objects/b863174b1147a6a9d9ab4922d9ced4eee19452ac/client.jar"
