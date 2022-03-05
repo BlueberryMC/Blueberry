e96106f8fc3181db7302df7a1a2bb33cb053b9b4
acrylic-style <me@acrylicstyle.xyz>
2022-03-05 13:22:08 +0900

    (finally) Update to 1.18.2

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-export version=1.18.2-rc1
+export version=1.18.2
 export basedir="$(pwd -P)"
-export clientJarUrl="https://launcher.mojang.com/v1/objects/a2699aadf0435f35cc0694d2f9e9403baac27ee2/client.jar"
+export clientJarUrl="https://launcher.mojang.com/v1/objects/2e9a3e3107cca00d6bc9c97bf7d149cae163ef21/client.jar"
