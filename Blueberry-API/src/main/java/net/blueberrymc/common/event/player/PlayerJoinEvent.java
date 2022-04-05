4f0278c4e3dad49462ed0e13e995936d39d16923
acrylic-style <me@acrylicstyle.xyz>
2022-04-06 01:33:32 +0900

    Add PlayerJoinEvent

---
+++ Blueberry-API/src/main/java/net/blueberrymc/common/event/player/PlayerJoinEvent.java
@@ -0,0 +1,27 @@
+package net.blueberrymc.common.event.player;
+
+import net.blueberrymc.common.bml.event.Event;
+import net.minecraft.server.level.ServerPlayer;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+/**
+ * Fired when a player joins the server. This event is called after the player has been added to the level.
+ */
+public class PlayerJoinEvent extends Event {
+    private final ServerPlayer player;
+
+    public PlayerJoinEvent(@NotNull ServerPlayer player) {
+        this.player = Objects.requireNonNull(player, "player cannot be null");
+    }
+
+    /**
+     * Gets the player that joined the server.
+     * @return the player
+     */
+    @NotNull
+    public ServerPlayer getPlayer() {
+        return player;
+    }
+}
