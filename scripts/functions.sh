14dc54b5819292a02b3632d6d24fbf2d6f36c1ab
acrylic-style <me@acrylicstyle.xyz>
2022-06-08 01:35:46 +0900

    Remove all legacy scripts

--- scripts/functions.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-export version=1.18.2
-export basedir="$(pwd -P)"
-export clientJarUrl="https://launcher.mojang.com/v1/objects/2e9a3e3107cca00d6bc9c97bf7d149cae163ef21/client.jar"
