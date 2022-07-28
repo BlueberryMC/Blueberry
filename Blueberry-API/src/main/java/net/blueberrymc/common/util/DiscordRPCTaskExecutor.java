3e86dac85f8cfc64232ccd1f96efa0f4f9b03f25
acrylic-style <me@acrylicstyle.xyz>
2022-07-28 13:05:26 +0900

    Back-backport from 1.18.2

--- Blueberry-API/src/main/java/net/blueberrymc/common/util/DiscordRPCTaskExecutor.java
+++ Blueberry-API/src/main/java/net/blueberrymc/common/util/DiscordRPCTaskExecutor.java
@@ -174,7 +174,8 @@ public class DiscordRPCTaskExecutor {
                                 }
                                 lobbyManager.connectNetwork(lobby);
                                 lobbyManager.openNetworkChannel(lobby, NETWORK_SYSTEM_MESSAGE_ID, true);
-                                byte[] data = Component.Serializer.toJson(Component.literal("")).getBytes(StandardCharsets.UTF_8);
+                                // TODO: translate
+                                byte[] data = Component.Serializer.toJson(new TextComponent(tag + " joined the lobby!").withStyle(ChatFormatting.YELLOW)).getBytes(StandardCharsets.UTF_8);
                                 for (DiscordUser user : lobbyManager.getMemberUsers(lobby)) {
                                     lobbyManager.sendNetworkMessage(lobby, user.getUserId(), NETWORK_SYSTEM_MESSAGE_ID, data);
                                 }
