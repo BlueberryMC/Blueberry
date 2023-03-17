4693f18054bbd2f165d98fcd94684a0b122d248d
acrylic-style <me@acrylicstyle.xyz>
2023-03-17 14:52:51 +0900

    render background for GenerateModScreen

--- Blueberry-API/src/main/java/net/blueberrymc/client/gui/screens/GenerateModScreen.java
+++ Blueberry-API/src/main/java/net/blueberrymc/client/gui/screens/GenerateModScreen.java
@@ -1,9 +1,11 @@
 package net.blueberrymc.client.gui.screens;
 
+import com.mojang.blaze3d.vertex.PoseStack;
 import net.blueberrymc.common.resources.BlueberryText;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.CommonComponents;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class GenerateModScreen extends BlueberryScreen {
@@ -21,6 +23,11 @@ public class GenerateModScreen extends BlueberryScreen {
                 Button.builder(CommonComponents.GUI_BACK, (button) -> this.minecraft.setScreen(previousScreen))
                         .bounds(this.width / 2 - 50, this.height - 38, 96, 20)
                         .build());
-        super.init();
+    }
+
+    @Override
+    public void render(@NotNull PoseStack poseStack, int mouseX, int mouseY, float deltaFrameTime) {
+        renderBackground(poseStack);
+        super.render(poseStack, mouseX, mouseY, deltaFrameTime);
     }
 }
